abfbf5a5811d660e35df1e440a24b81f4384d543
nejcokorn <nejc.okorn@gmail.com>
2026-02-01 10:17:30 +0100

    Add delay delete nodes

---
+++ ha-delete-delay-port/ha-delete-delay-port.js
@@ -0,0 +1,34 @@
+module.exports = function(RED) {
+	function DeleteDelayByPort(config) {
+		RED.nodes.createNode(this, config);
+		this.agent = config.agent;
+		this.deviceId = config.deviceId;
+		this.actionPort = config.actionPort;
+		this.agentNode = RED.nodes.getNode(config.agent);
+
+		this.on('input', async (msg, send, done) => {
+			try {
+				if (!this.agentNode) {
+					return done(Error("Configure Home Automation agent."));
+				}
+				
+				const response = await fetch(`${this.agentNode.url}/can/${this.agentNode.canbus}/device/${this.deviceId}/delay/port/${this.actionPort}`,
+					{
+						method: "DELETE"
+					}
+				);
+				const json = await response.json();
+
+				if (json.success == true) {
+					send({ payload: json.data });
+				} else {
+					done(json.error);
+				}
+
+			} catch (error) {
+				done(error);
+			}
+		});
+	}
+	RED.nodes.registerType("ha-delete-delay-port", DeleteDelayByPort);
+}
