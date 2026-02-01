6f37e9e4df7e49f57891ca67edfaa6c79775540e
nejcokorn <nejc.okorn@gmail.com>
2026-02-01 09:38:27 +0100

    Fix variable names passed to the request

--- ha-set-port/ha-set-port.js
+++ ha-set-port/ha-set-port.js
@@ -1,9 +1,11 @@
 module.exports = function(RED) {
 	function SetPort(config) {
 		RED.nodes.createNode(this, config);
-		this.deviceId = config.deviceId;
-		this.port = config.port;
 		this.agent = config.agent;
+		this.deviceId = config.deviceId;
+		this.actionType = config.actionType;
+		this.actionPort = config.actionPort;
+		this.actionDelay = config.actionDelay;
 		this.agentNode = RED.nodes.getNode(config.agent);
 
 		this.on('input', async (msg, send, done) => {
@@ -12,16 +14,15 @@ module.exports = function(RED) {
 					return done(Error("Configure Home Automation agent."));
 				}
 				
-				const response = await fetch(
-					`${this.agentNode.url}/can/${this.agentNode.canbus}/device/${this.deviceId}/digital/output/${this.port}`,
+				const response = await fetch(`${this.agentNode.url}/can/${this.agentNode.canbus}/device/${this.deviceId}/digital/output/${this.actionPort}`,
 					{
 						method: "POST",
 						headers: {
 							"Content-Type": "application/json"
 						},
 						body: JSON.stringify({
-							type: this.type,
-							delay: this.delay
+							type: this.actionType,
+							delay: this.actionDelay
 						})
 					}
 				);
